b64edd5219a2e2596f6220f3ef5c2f9293f1f767
rabik0135 <ivan.rabinchuk@gmail.com>
2024-10-07 21:07:08 +0300

    added humanReadableSize function

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,23 @@
 import java.io.File;
+import java.util.HashMap;
+import java.util.concurrent.ForkJoinPool;
 
 public class Main {
     public static void main(String[] args) {
         String folderPath = "D:/University";
         File file = new File(folderPath);
 
-        System.out.println(getFolderSize(file));
+        long start = System.currentTimeMillis();
+
+        FolderSizeCalculator calculator = new FolderSizeCalculator(file);
+        ForkJoinPool pool = new ForkJoinPool();
+        long size = pool.invoke(calculator);
+        System.out.println(size);
+        //System.out.println(getFolderSize(file));
+
+        long duration = System.currentTimeMillis() - start;
+        System.out.println(duration + " ms");
+        System.out.println(getHumanReadableSize(size));
     }
 
     public static long getFolderSize(File folder) {
@@ -21,4 +33,33 @@ public class Main {
         return sum;
     }
 
+    public static String getHumanReadableSize(long size) {
+        int counter = 0;
+        while (size > 1024) {
+            size = size / 1024;
+            counter++;
+        }
+        size += 1;
+
+        HashMap<Integer, String> map = new HashMap<>() {{
+            put(0, "B");
+
+            put(1, "Kb");
+
+            put(2, "Mb");
+
+            put(3, "Gb");
+
+            put(4, "Tb");
+        }};
+
+        StringBuilder builder = new StringBuilder();
+        builder.append(size).append(map.get(counter));
+
+        return builder.toString();
+    }
+
+    public static long getSizeFromHumanReadable(String size) {
+        return 0;
+    }
 }
